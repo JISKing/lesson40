2ba06918479553d6c316cbf528310f46a41a914e
David Larchikov <davidlarchikov@gmail.com>
2023-04-05 18:08:04 +0300

    lesson40

--- src/components/Article.js
+++ src/components/Article.js
@@ -6,26 +6,43 @@ function Article() {
       <div className="article__title">
         <h2>NVIDIA Accelerated AI on Azure</h2>
       </div>
-      <div className="article__description">
-        <h2>Article description:</h2>
-        <p>
-          NVIDIA on Azure is bringing AI, networking, and high-perfomance
-          computing to the enterprise
-        </p>
-      </div>
-      <div className="buttonCon">
-      <button>Read</button>
-      <button>Mark as read</button>
-      <button>Mark as unread</button>
-      </div>
-      <div className="authorsCon">
-      <div>Author: Mike</div>
-      <div>Published: 06.12.2022</div>
-      <div>Theme: Video cards</div>
-    </div>
+      <ArticleBody />
+      <ArticleActions />
+      <ArticleAuthor />
     </div>
   );
 }
 
+function ArticleBody() {
+  return (
+    <div className="article__description">
+      <h2>Article description:</h2>
+      <p>
+        NVIDIA on Azure is bringing AI, networking, and high-perfomance
+        computing to the enterprise
+      </p>
+    </div>
+  );
+}
+
+function ArticleActions() {
+  return (
+    <div className="buttonCon">
+      <button>Read</button>
+      <button>Mark as read</button>
+      <button>Mark as unread</button>
+    </div>
+  );
+}
+
+function ArticleAuthor() {
+  return (
+    <div className="authorsCon">
+      <div>Author: Mike</div>
+      <div>Published: 06.12.2022</div>
+      <div>Theme: Video cards</div>
+    </div>
+  );
+}
 
 export default Article;
